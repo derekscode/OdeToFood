d81b04bcf68f34c22a8c64bcab7f329e8dbdd717
derekscode <derekscode@github.com>
2016-03-25 16:49:04 -0700

    stopped at "Attribute Routes"

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OdeToFood.Services;
@@ -9,7 +10,7 @@ namespace OdeToFood
 {
     public class Startup
     {
-        public Startup()
+        public Startup() 
         {
             var builder = new ConfigurationBuilder()
                                     .AddJsonFile("appsettings.json");
@@ -24,6 +25,7 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -31,15 +33,35 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
+            IHostingEnvironment environment,
             IGreeter greeter)
         {
             app.UseIISPlatformHandler();
+
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRuntimeInfoPage("/info");
+
+            app.UseFileServer();
+
+            app.UseMvc(ConfigureRoute);
 
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetGreeting();
                 await context.Response.WriteAsync(greeting);
             });
+
+        }
+
+        private void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            // /Home/Index/
+            routeBuilder.MapRoute("Default", 
+                "{controller=Home}/{action=Index}/{id?}");
         }
 
         // Entry point for the application.
